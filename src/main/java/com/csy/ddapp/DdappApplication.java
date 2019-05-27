76a2db6fca8ed35ebf50e8818b4112d11575eeab
chensiyou <905630790@qq.com>
2019-05-27 19:04:31 +0800

    feat:"初始化"

---
+++ src/main/java/com/csy/ddapp/DdappApplication.java
@@ -0,0 +1,13 @@
+package com.csy.ddapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdappApplication.class, args);
+    }
+
+}
